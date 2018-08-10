50d64c8da85defac6f3f55d7dac4ec91067f8b60
lx <chentao@codemao.cn>
2018-08-10 18:27:17 +0800

    style: modify old code style

--- src/ast/static_optimize.js
+++ src/ast/static_optimize.js
@@ -5,80 +5,78 @@ const genStaticKeysCached = _.cached(genStaticKeys)
 let isStaticKey, isPlatformReservedTag
 
 export function optimize (astRoot, options = {}) {
-	if (!astRoot.root) return
-	isStaticKey = genStaticKeysCached(options.staticKeys || '')
-	isPlatformReservedTag = _.isReservedTag || _.no
+  if (!astRoot.root) return
+  isStaticKey = genStaticKeysCached(options.staticKeys || '')
+  isPlatformReservedTag = _.isReservedTag || _.no
 
-	// 第一遍：标记所有非静态节点
-	markStatic(astRoot)
-	// 第二遍：标记静态根节点
-	markStaticRoots(astRoot, false)
+  // 第一遍：标记所有非静态节点
+  markStatic(astRoot)
+  // 第二遍：标记静态根节点
+  markStaticRoots(astRoot, false)
 }
 
 function markStatic (node) {
-	node.static = isStatic(node)
-	if (node.type === TAG) {
-		if (!node.attrs.length && !node.direction.length) {
-			node.plain = true
-		}
-		if (
-				!isPlatformReservedTag(node.tagName) &&
-				!_.isInserComponents(node.tagName) &&
-				node.attrs['inline-template'] == null
-		) {
-			return
-		}
+  node.static = isStatic(node)
+  if (node.type === TAG) {
+    if (!node.attrs.length && !node.direction.length) {
+      node.plain = true
+    }
+    if (
+        !isPlatformReservedTag(node.tagName) &&
+        !_.isInserComponents(node.tagName) &&
+        node.attrs['inline-template'] == null
+    ) return
 
-		_.each(node.children, (child, i) => {
-			markStatic(child)
-			if (!child.static) {
-				node.static = false
-			}
-		})
-	}
+    _.each(node.children, child => {
+      markStatic(child)
+      if (!child.static) {
+        node.static = false
+      }
+    })
+  }
 }
 
 function markStaticRoots (node, isInFor) {
-	if (node.type === TAG) {
-		if (node.static) {
-			node.staticInFor = isInFor
-		}
-		// 对于一个有资格作为静态跟节点的元素，他应该有children
-		// 而且子元素不应该是一个纯静态文本，否则消耗会超过获得的收益
-		// 更好的做法让它每次渲染时都刷新
-		if (node.static && node.children.length && !(
-			node.children.length === 1 &&
-			node.children[0].type === TEXT
-		)) {
-			node.staticRoot = true
-			return
-		} else {
-			node.staticRoot = false
-		}
-		if (node.children.length) {
-			_.each(node.children, (child, i) => {
-				markStaticRoots(child, isInFor || !!node.for)
-			})
-		}
-	}
+  if (node.type === TAG) {
+    if (node.static) {
+      node.staticInFor = isInFor
+    }
+    // 对于一个有资格作为静态跟节点的元素，他应该有children
+    // 而且子元素不应该是一个纯静态文本，否则消耗会超过获得的收益
+    // 更好的做法让它每次渲染时都刷新
+    if (node.static && node.children.length && !(
+        node.children.length === 1 &&
+        node.children[0].type === TEXT
+    )) {
+      node.staticRoot = true
+      return
+    } else {
+      node.staticRoot = false
+    }
+    if (node.children.length) {
+      _.each(node.children, child => {
+        markStaticRoots(child, isInFor || !!node.for)
+      })
+    }
+  }
 }
 
 function isStatic (node) {
-	if (node.type === STATICTAG) {
-		return false
-	}
-	if (node.type === TEXT) {
-		return true
-	}
+  if (node.type === STATICTAG) {
+    return false
+  }
+  if (node.type === TEXT) {
+    return true
+  }
 
-	return !!(
-		!node.hasBindings() &&
-		!_.isBuiltInTag(node.tagName) &&
-		!node.if && !node.for &&
-		isPlatformReservedTag(node.tagName) &&
-		!isDirectChildOfTemplateFor(node) &&
-		Object.keys(node).every(isStaticKey)
-	)
+  return !!(
+    !node.hasBindings() &&
+    !_.isBuiltInTag(node.tagName) &&
+    !node.if && !node.for &&
+    isPlatformReservedTag(node.tagName) &&
+    !isDirectChildOfTemplateFor(node) &&
+    Object.keys(node).every(isStaticKey)
+  )
 }
 
 // template的直接子元素且在for中
